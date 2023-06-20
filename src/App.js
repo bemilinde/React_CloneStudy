fa652ba63460b6b076462f2325fe8819368dc7fc
bemilinde <bemilinde@gmail.com>
2023-06-20 10:38:31 +0900

    06/20

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 /* eslint-disable */
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import data from './data.js'
 import Detail from './routes/Detail.js'
+import Cart from './routes/Cart.js'
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import axios from 'axios';
+
+export let Context1 = createContext();
+
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
+  let [재고] = useState([10, 11, 12])
 
   return (
     <div className="App">
@@ -19,7 +25,7 @@ function App() {
           <Navbar.Brand href="#home">NEGORANI</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail/0')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -41,9 +47,30 @@ function App() {
                   }        
                 </div>
             </div>
+            <button onClick={()=>{
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result)=>{
+                let copy = [...shoes, ...result.data];
+                setShoes(copy);
+                
+              })
+              .catch(()=>{
+                console.log('실패')
+              })
+
+
+
+            }}>더보기</button>
           </>
         }/>
-        <Route path='/detail/:id' element={<Detail shoes={shoes}/>}/>
+        <Route path='/detail/:id' element={
+          <Context1.Provider value={{ 재고, shoes}}>
+           <Detail shoes={shoes}/>
+          </Context1.Provider>
+        }/>
+
+        <Route path="/cart" element= <Cart/>/>
+
 
         <Route path="/about" element={ <About/> } >  
           <Route path="member" element={ <div>멤버들</div> } />
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,52 +1,105 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { cleanup } from "@testing-library/react";
+import { Nav } from "react-bootstrap";
+import '../App.css';
+
+import { Context1 } from './../App.js'
 
 
 
 function Detail(props){
 
-    let {id} = useParams();
-    let [count, setCount] = useState(0)
-    let [alert, setAlert] = useState(true)
+    let {재고} = useContext(Context1);
 
+    let {id} = useParams();
+    let 찾은상품 = props.shoes.find(x => x.id == id);    
+    let [alert, setAlert] = useState(true)
+    let [Dfade, DsetFade] = useState('')
+    let [탭, 탭변경] = useState(0)
 
     useEffect(()=>{
-        let time = setTimeout(()=>{ setAlert(false) }, 2000);
-
+        let a = setTimeout(()=>{ }, 2000)
         return ()=>{
-            clearTimeout(time)
+            clearTimeout(a)
+        }
+      }, [])
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ DsetFade('end') },100)
+        return ()=>{
+            clearTimeout(a)
+            DsetFade('')
         }
     }, [])
 
 
 
-    return(
-    <div className="container">
-        {
-            alert == true
-            ? <div id = "aa" className="alert alert-warning">
-                2초 이내 구매시 할인
-              </div>
-            : null
-        }
-
-        {count}
-        <button onClick={()=>{ setCount(count+1) }}>버튼</button>
-        <div className="row">
+      return (
+        <div className={'container start ' + Dfade}>
+            {
+                alert == true
+                ? <div className="alert alert-warning">
+                    2초이내 구매시 할인
+                  </div>
+                : null
+            }
+          
+          <div className="row mb-5">
             <div className="col-md-6">
               <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </div>
-            <div className="col-md-6">
-                <h4 className="pt-5">{props.shoes[id].title}</h4>
-                <p>{props.shoes[id].content}</p>
-                <p>{props.shoes[id].price}원</p>
-                <button className="btn btn-danger">주문하기</button> 
+            <div className="col-md-6 mt-4">
+              <h4 className="pt-5">{찾은상품.title}</h4>
+              <p>{찾은상품.content}</p>
+              <p>{찾은상품.price}원</p>
+              <button className="btn btn-danger">주문하기</button> 
             </div>
-        </div>
-    </div> 
-    )
+          </div>
+
+        <Nav variant="tabs"  defaultActiveKey="link0">
+            <Nav.Item>
+              <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={()=>{탭변경(1)}} eventKey="link1">버튼1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+            </Nav.Item>
+        </Nav>
+        <TabContent 탭 = {탭}/>
+
+      </div>  
+      )
+};
+
+function TabContent({탭}){
+
+    let [fade, setFade] = useState('')
+    let {재고} = useContext(Context1);
+
+    useEffect(()=>{    
+        let a = setTimeout(()=>{ setFade('end') },100)
+        return ()=>{
+            clearTimeout(a)
+            setFade('')
+        }
+    }, [탭])
+
+    return (<div className={`start ${fade}` }>{
+        [<div>{재고}</div>, <div>내용1</div>, <div>내용2</div>][탭]
+    }
+    </div>)
+
+    // if(탭 == 0){
+    //    return <div>내용0</div>
+    // }else if(탭 == 1){
+    //    return <div>내용1</div>
+    // }else if(탭 == 2){
+    //    return <div>내용2</div>
+    // }
 }
 
 export default Detail;
